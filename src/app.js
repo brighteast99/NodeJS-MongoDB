6eb2c83ce02fe57d26dc0295aa823e8fd94852c0
brighteast99 <brighteast99@gmail.com>
2023-07-16 14:24:08 +0900

    practice: form post 기초

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-var path = require("path");
+app.use(express.urlencoded({ extended: true }));
 
+var path = require("path");
 app.listen(8080, function () {
   console.log("listening on 8080");
 });
@@ -12,4 +13,12 @@ app.get("/", (req, res) => {
 
 app.get("/new", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/pages/new.html"));
+});
+
+app.post("/add", (req, res) => {
+  console.log(req.body);
+  if (!req.body.taskName || !req.body.taskDueDate)
+    return res.status(400).send("정보 부족");
+
+  res.send("전송완료");
 });
