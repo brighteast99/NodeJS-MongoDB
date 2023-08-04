788ee60eb549750277d1f5969c54fb2596f63e30
brighteast99 <brighteast99@gmail.com>
2023-08-05 00:33:00 +0900

    practice: transaction 추가

--- modules/db/index.js
+++ modules/db/index.js
@@ -4,6 +4,8 @@ class MongoDB {
 	constructor() {
 		this.client = null;
 		this.db = null;
+		this.session = null;
+		this.options = {};
 	}
 
 	connect(URL) {
@@ -34,17 +36,36 @@ class MongoDB {
 				.close()
 				.then(() => {
 					this.client = null;
+					this.db = null;
 					resolve();
 				})
 				.catch((err) => reject(err));
 		});
 	}
 
-	disconnect() {
-		if (!this.client) return;
-		this.client.close();
-		this.client = null;
-		this.db = null;
+	startTransaction() {
+		if (!this.client) throw new Error("MongoDB client is not initialized");
+		if (this.session) throw new Error("Transaction already started");
+
+		this.session = this.client.startSession();
+		this.session.startTransaction();
+		this.options = { session: this.session };
+	}
+
+	async commitTransaction() {
+		if (!this.session) return;
+		await this.session.commitTransaction();
+		this.session.endSession();
+		this.session = null;
+		this.options = {};
+	}
+
+	abortTransaction() {
+		if (!this.session) return;
+		this.session.abortTransaction();
+		this.session.endSession();
+		this.session = null;
+		this.options = {};
 	}
 
 	insertOne(collection, data) {
@@ -52,7 +73,7 @@ class MongoDB {
 			try {
 				this.db
 					.collection(collection)
-					.insertOne(data)
+					.insertOne(data, this.options)
 					.then((result) => {
 						resolve(result);
 					});
@@ -67,7 +88,7 @@ class MongoDB {
 			try {
 				this.db
 					.collection(collection)
-					.insertMany(data)
+					.insertMany(data, this.options)
 					.then((result) => {
 						resolve(result);
 					});
@@ -113,7 +134,7 @@ class MongoDB {
 			try {
 				this.db
 					.collection(collection)
-					.updateOne(condition, { $set: data })
+					.updateOne(condition, { $set: data }, this.options)
 					.then((result) => {
 						resolve(result.modifiedCount > 0);
 					});
@@ -128,7 +149,7 @@ class MongoDB {
 			try {
 				this.db
 					.collection(collection)
-					.deleteOne(condition)
+					.deleteOne(condition, this.options)
 					.then((result) => {
 						resolve(result.deletedCount > 0);
 					});
@@ -143,7 +164,7 @@ class MongoDB {
 			try {
 				this.db
 					.collection(collection)
-					.deleteMany(condition)
+					.deleteMany(condition, this.options)
 					.then((result) => {
 						resolve(result.deletedCount > 0);
 					});
