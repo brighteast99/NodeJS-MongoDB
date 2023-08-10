7115bc0bbc5070a84eab49629c8c170b0f69ba9e
brighteast99 <brighteast99@gmail.com>
2023-08-11 01:35:29 +0900

    practice: socket.io

--- app.js
+++ app.js
@@ -6,26 +6,29 @@ const passportConfig = require("./modules/auth/passport");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const app = express();
+const http = require("http").createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(http);
 let server;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({
-      mongoUrl: process.env.DB_URL,
-    }),
-  })
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false,
+		store: MongoStore.create({
+			mongoUrl: process.env.DB_URL,
+		}),
+	})
 );
 app.use(passport.initialize());
 app.use(passport.session());
 passportConfig();
 app.use((req, res, next) => {
-  if (!req.isAuthenticated()) res.clearCookie("connect.sid");
-  next();
+	if (!req.isAuthenticated()) res.clearCookie("connect.sid");
+	next();
 });
 app.use("/public", express.static("public"));
 app.use("/", require("./routes"));
@@ -33,25 +36,41 @@ app.set("view engine", "ejs");
 
 const mongoDB = require("./modules/db");
 mongoDB
-  .connect(process.env.DB_URL)
-  .then(() => {
-    console.log("Connected to MongoDB");
+	.connect(process.env.DB_URL)
+	.then(() => {
+		console.log("Connected to MongoDB");
 
-    server = app.listen(process.env.PORT, () => {
-      console.log(`listening on ${process.env.PORT}`);
-    });
-  })
-  .catch(() => {
-    console.log("Failed to connect to database!");
-  });
+		server = http.listen(process.env.PORT, () => {
+			console.log(`listening on ${process.env.PORT}`);
+
+			io.on("connect", (socket) => {
+				console.log(`user ${socket.id} connected`);
+				socket.on("message", (data) => {
+					io.emit(
+						"broadcast",
+						Object.assign({ dateCreated: new Date() }, data)
+					);
+				});
+				socket.on("disconnect", () => {
+					console.log(`user ${socket.id} disconnected`);
+					socket.disconnect();
+				});
+			});
+		});
+	})
+	.catch((err) => {
+		console.error(err);
+		console.log("Failed to connect to database!");
+	});
 
 function closeServer(signal) {
-  if (server) {
-    server.close(() =>
-      console.log(`Received ${signal} at ${new Date()}.\nClosing server.`)
-    );
-    mongoDB.close().then(() => console.log("Closed MongoDB connection."));
-  }
+	if (server) {
+		server.close(() =>
+			console.log(`Received ${signal} at ${new Date()}.\nClosing server.`)
+		);
+		mongoDB.close().then(() => console.log("Closed MongoDB connection."));
+		io.close();
+	}
 }
 process.on("SIGTERM", closeServer);
 process.on("SIGINT", closeServer);
